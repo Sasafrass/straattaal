3f03e8528fbe163cea837093a8c3b9ef9292bbb6
Raoul Grouls <r.grouls@anchormen.nl>
2021-07-11 21:07:51 +0200

    n_letters and all_letters, temp to 1

--- app/api/slang.py
+++ app/api/slang.py
@@ -4,16 +4,12 @@ from app.api import bp
 from app.ml_models.rnn.loaded_rnn_model import return_loaded_model
 from app.ml_models.rnn.generate import generate_word
 
-N_LETTERS = 27  # TODO: Fix the hardcoded number of letters.
-ALL_LETTERS = (
-    "vtdlcg bnuhrzmjskpefiçxoway"  # TODO: Fix the hardcoded set of ALL letters.
-)
-
 
 @bp.route("/generate_slang", methods=["GET"])
 def generate_slang():
     """Generate and return a new slang word."""
-    model = return_loaded_model()
+    model, ALL_LETTERS = return_loaded_model()
+    N_LETTERS = len(ALL_LETTERS) + 1
     
     new_word = generate_word(
         model=model,
@@ -21,7 +17,7 @@ def generate_slang():
         ALL_LETTERS=ALL_LETTERS,
         start_letter="l",  # TODO: Fix this to random letter.
         maxn=20,  # TODO: Fix this.
-        temp=0.5,  # TODO: Fix the temperature.
+        temp=1,  # TODO: Fix the temperature.
     )
 
     # TODO: Return a json containing the word.
