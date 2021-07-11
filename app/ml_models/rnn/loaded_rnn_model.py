1b96497ca2c8ea46437c0644fa47d590535df996
Sasafrass <albertharkema@gmail.com>
2021-07-11 13:40:39 +0200

    model loads successfully

---
+++ app/ml_models/rnn/loaded_rnn_model.py
@@ -0,0 +1,24 @@
+import os
+
+from app.ml_models.rnn.rnn_model import RNN
+
+import torch
+
+def return_loaded_model():
+    # Variables for trained RNN model.
+    n_hidden = 128
+    n_letters = 27 # TODO: Fix the hardcoded number of letters. 
+
+    # Construct correct path to model file.
+    # TODO: Fix the hardcoded cwd path!
+    cwd_path = os.path.abspath(os.getcwd())
+    cwd_path = os.path.join(cwd_path, "app", "ml_models", "rnn")
+    model_path = "2337-26062021_model.pth"
+    PATH = os.path.join(cwd_path, model_path)
+
+    # Actually load the model.
+    model = RNN(n_letters, n_hidden, n_letters) 
+    model.load_state_dict(torch.load(PATH))
+    model.eval()
+
+    return model
---
+++ app/ml_models/rnn/rnn_model.py
@@ -0,0 +1,25 @@
+import torch
+
+import torch.nn as nn
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+        self.tanh = nn.Tanh()
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.tanh(self.i2h(combined))
+        output = self.i2o(hidden)
+        output = self.dropout(output)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
