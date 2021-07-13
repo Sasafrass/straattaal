0e63b43a39edb892d6e49a9545eeb798bf43ff49
Sasafrass <albertharkema@gmail.com>
2021-07-13 22:56:39 +0200

    Implement groups model and update index.html accordingly

--- app/models.py
+++ app/models.py
@@ -3,12 +3,27 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+# Create many-to-many mapping from groups to users using a helper table.
+groups = db.Table(
+    'groups',
+    db.Column("group_id", db.Integer, db.ForeignKey("group.id"), primary_key=True),
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),    
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     words = db.relationship("Slang", backref="author", lazy="dynamic")
+    groups = db.relationship(
+        "Group", 
+        secondary=groups, 
+        lazy="subquery",
+        backref=db.backref("groups", lazy=True),
+        overlaps="users, groups",
+    )
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -28,6 +43,18 @@ class Slang(db.Model):
 
     def __repr__(self):
         return f"<Word {self.word}>"
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True, unique=True)
+    users = db.relationship(
+        "User", 
+        secondary=groups, 
+        lazy="subquery",
+        backref=db.backref("users", lazy=True),
+        overlaps="groups, users",
+    )
 
 
 @login.user_loader
