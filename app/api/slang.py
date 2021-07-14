0586cc7cd7a788ed99c95f702d2b0055233525d4
Anna <annaproxy@protonmail.com>
2021-07-14 23:20:21 +0200

    app can now use Anna's model

--- app/api/slang.py
+++ app/api/slang.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from app.api import bp
 
-from app.ml_models.rnn.loaded_rnn_model import return_loaded_model
+from app.ml_models.rnn.loaded_rnn_model import return_loaded_model, load_model
 from app.ml_models.rnn.helpers import random_choice
 from app.ml_models.rnn.generate import generate_word
 
@@ -9,16 +9,18 @@ from app.ml_models.rnn.generate import generate_word
 @bp.route("/generate_slang", methods=["GET"])
 def generate_slang():
     """Generate and return a new slang word."""
-    model, ALL_LETTERS = return_loaded_model()
-    N_LETTERS = len(ALL_LETTERS) + 1
+
+    # TODO Should not load model every time a word is queried
+    # I know nothing of flask, can we save the model upon starting the app?
+
+    model, dataset = load_model()
 
     new_word = generate_word(
         model=model,
-        N_LETTERS=N_LETTERS,
-        ALL_LETTERS=ALL_LETTERS,
-        start_letter=random_choice(ALL_LETTERS),
-        maxn=20,  # TODO: Fix this.
-        temp=0.3,
+        dataset=dataset,
+        start_letter='random',
+        max_len=20,  # TODO: Fix this.
+        temperature=0.3,
     )
 
     # TODO: Return a json containing the word.
