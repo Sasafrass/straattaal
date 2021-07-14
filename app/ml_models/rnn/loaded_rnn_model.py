7823e860a1db866904c7d4f86e28109d613e7a2d
Anna <annaproxy@protonmail.com>
2021-07-14 23:09:25 +0200

    Add functionality to load Anna's Model

--- app/ml_models/rnn/loaded_rnn_model.py
+++ app/ml_models/rnn/loaded_rnn_model.py
@@ -1,6 +1,20 @@
 import os
 import torch
-from app.ml_models.rnn.rnn_model import RNN
+from app.ml_models.rnn.rnn_model import RNN, RNNAnna
+from app.ml_models.rnn.data_tools import WordLevelDataset
+
+
+def load_model(model_name='pretrained_NL_epoch5.pt'):
+    path = os.path.join(os.path.abspath(os.getcwd()),
+                        "app", "ml_models", "rnn")
+    path = os.path.join(path, model_name)
+    dataset = WordLevelDataset('data/', 'straattaal.txt')
+    # TODO: Fix hardcoded hidden size
+    m = RNNAnna(dataset.vocabulary_size, 128)
+    checkpoint = torch.load(path)
+    m.load_state_dict(checkpoint['model_state_dict'])
+    m.eval()
+    return m, dataset
 
 
 def return_loaded_model():
