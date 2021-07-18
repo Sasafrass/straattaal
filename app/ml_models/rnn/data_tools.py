a4710b052233004d56e62dfddc7ffda3167f146a
Anna <annaproxy@protonmail.com>
2021-07-18 15:13:52 +0200

    Improve docstring for convert_to_string, but this won't matter once vocab object is there

--- app/ml_models/rnn/data_tools.py
+++ app/ml_models/rnn/data_tools.py
@@ -43,12 +43,12 @@ class WordLevelDataset(Dataset):
         s2 = [self.char_to_idx_dict[z] for z in list(self.words[i]) + ["<EOS>"]]
         return torch.LongTensor(s1), torch.LongTensor(s2)
 
-    def convert_to_string(self, char_ix):
+    def convert_to_string(self, char_idxs):
         """Convert an indexed representation of the characters to the actual characters.
 
         Args:
-            char_ix: ?Iterable? with indices that represent characters.
+            char_ix: Iterable of ints (indices) that represent characters.
         """
 
-        result = "".join(self.idx_to_char_dict[ix] for ix in char_ix)
+        result = "".join(self.idx_to_char_dict[ix] for ix in char_idxs)
         return result
