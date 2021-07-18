35ea578ed94f65f73cf200e9a5830dd7ccc6e252
Sasafrass <albertharkema@gmail.com>
2021-07-18 21:35:04 +0200

    Implement initial routes and templates for user subsystem.

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,7 +1,19 @@
-from flask import render_template
+from flask import redirect, render_template, url_for
+from flask_login import current_user, login_required
+from app.models import User
 from app.users import bp
 
 
 @bp.route("/main")
+@login_required
 def main():
-    return "Main users page."
+    # Automatically redirect to the current user's profile.
+    return redirect(url_for("users.users", username=current_user.username))
+
+
+@bp.route("/<username>")
+@login_required
+def users(username):
+    user = User.query.filter_by(username=username).first_or_404()
+
+    return render_template("users/user.html", user=user)
