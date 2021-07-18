4b96f4cdb2e1571b308505e334e5d3c9cfee5615
Sasafrass <albertharkema@gmail.com>
2021-07-18 18:04:05 +0200

    Create meaning form.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class GenerateSlangForm(FlaskForm):
-    submit = SubmitField("Generate Slang")
+    """Submit form to generate a new slang word."""
+    submit_generate = SubmitField("Generate Slang")
 
 
+class MeaningForm(FlaskForm):
+    """Submit form to persist your word and meaning to database."""
+    word = StringField("Word", validators=[DataRequired()])
+    meaning = StringField("Meaning")
+    submit_meaning = SubmitField("Save Word And Meaning")
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,22 +1,43 @@
 import requests
 from flask import render_template
 from app.main import bp
-from app.main.forms import GenerateSlangForm
+from app.main.forms import GenerateSlangForm, MeaningForm
 
 
 @bp.route("/", methods=["GET", "POST"])
 @bp.route("/index", methods=["GET", "POST"])
 def index():
-    form = GenerateSlangForm()
+    generate_slang_form = GenerateSlangForm()
+    meaning_form = MeaningForm()
 
-    if form.validate_on_submit():
+    if generate_slang_form.submit_generate.data and generate_slang_form.validate():
         # TODO: Might be overkill to use an API call here rather than internal call.
         response = requests.get("http://localhost:5000/api/generate_slang")
         response = response.json()
         slang_word = response["slang_word"]
+        meaning_form.word.data = slang_word
 
         return render_template(
-            "index.html", title="Home", slang_word=slang_word, form=form
+            "index.html", 
+            title="Home", 
+            slang_word=slang_word, 
+            generate_slang_form=generate_slang_form,
+            meaning_form=meaning_form,
         )
 
-    return render_template("index.html", title="Home", slang_word="", form=form)
+    if meaning_form.submit_meaning.data and meaning_form.validate():
+        print("Successfully submitted the meaning form")
+
+        return render_template(
+            "index.html", 
+            title="Home", 
+            generate_slang_form=generate_slang_form,
+            meaning_form=meaning_form,
+        )
+
+    return render_template(
+        "index.html", 
+        title="Home", 
+        slang_word="", 
+        generate_slang_form=generate_slang_form,
+        meaning_form=meaning_form)
