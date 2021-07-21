9b9a26e0dde150942330f4368add382a2796fe98
Anna <annaproxy@protonmail.com>
2021-07-21 18:50:50 +0200

    Update argparser with more training options. Fix some paths

--- train_wrapper.py
+++ train_wrapper.py
@@ -1,5 +1,6 @@
 """Functionality for locally training files."""
 import argparse
+import os
 from typing import List
 
 import torch
@@ -9,44 +10,66 @@ from torch.utils.data import DataLoader
 from app.ml_models.rnn.data_tools import WordLevelDataset
 from app.ml_models.rnn.vocabulary import Vocabulary
 from app.ml_models.rnn.train import train
+from app.ml_models.rnn.loaded_rnn_model import init_torch_model_from_path
 from app.ml_models.rnn.rnn_model import RNNAnna
 
 
-def run(
-    filename_source: str = "data/familienamen.txt",
-    filename_datasets: List[str] = ["familienamen.txt"],
-    name: str = "familienamen",
-    device: str = "cpu",
-):
-    """Run a simple training loop.
+def _run(**kwargs):
+    """Run a simple training loop."""
+    # Preliminary checks
+    if kwargs["batch_size"] != 1:
+        raise ValueError(
+            "Batch size 1 is not implemented. Want to help out? https://github.com/Sasafrass/straattaal/issues/22"
+        )
 
-    Args:
-        filename_source: File to pull the training data from.
-        filename_datasets: Files to pull the datasets from.
-        name: Name to pass to the actual train function.
-        device: Device to run the network on: cpu or cuda/gpu.
-    """
+    # Build or load vobaulary
+
     v = Vocabulary()
-    v.build(
-        prefix=".",
-        filename_source=filename_source,
-        filename_destination="vocabulary_extended.txt",
-    )
+    try:
+        v.load(prefix=kwargs["data_directory"], filename_vocab=kwargs["filename_vocab"])
+    except FileNotFoundError:
+        v.build(
+            prefix=kwargs["data_directory"],
+            filename_datasets=kwargs["filename_datasets"],
+            filename_destination=kwargs["filename_vocab"]
+        )
+
+    # Build training set and dataloader.
     dataset = WordLevelDataset(
-        prefix="data/", filename_datasets=filename_datasets, vocabulary=v
+        prefix=kwargs["data_directory"],
+        filename_datasets=kwargs["filename_datasets"],
+        vocabulary=v,
     )
-    # Currently only batch size 1 works
-    train_loader = DataLoader(dataset, 1, shuffle=True)
-    rnn = RNNAnna(dataset.vocabulary.size, hidden_size=128)
+
+    if kwargs["num_training_steps"] is not None:
+        # Steps = epochs * (data set size / batch size)
+        # Hence Epochs = steps / (data set size / batch size)
+        kwargs["epochs"] = kwargs["num_training_steps"] // (
+            len(dataset) / kwargs["batch_size"]
+        )
+
+    train_loader = DataLoader(dataset, kwargs["batch_size"], shuffle=True)
+
+    # Set the stage
+    if not os.path.exists("models"):
+        os.makedirs("models")
+
+    # Build or load model
+    if kwargs["model_startpoint"] is not None:
+        rnn = init_torch_model_from_path(
+            kwargs["model_startpoint"], v, device=kwargs["device"]
+        )
+    else:
+        rnn = RNNAnna(dataset.vocabulary.size, hidden_size=kwargs["hidden_size"])
+
+    # Train.
     train(
         rnn,
         train_loader,
         dataset,
-        epochs=5,
-        device=device,
-        name=name,
-        save_every=1,
         print_every=3000,
+        save_directory="models",
+        **kwargs,
     )
 
 
@@ -62,25 +85,73 @@ if __name__ == "__main__":
     PARSER.add_argument(
         "--filename_datasets",
         nargs="+",
-        default=["familienamen.txt"],
+        default=["dutch.txt"],
         help="Files to pull the datasets from. Can be passed as space separated list.",
     )
     PARSER.add_argument(
-        "--filename_source",
+        "--filename_vocab",
         type=str,
-        default="data/familienamen.txt",
-        help="File to pull the training data from.",
+        default="vocabulary.txt",
+        help="File to get the vocabulary from. If the file does not exist, it will be created from the given train sets.",
     )
     PARSER.add_argument(
-        "--name",
+        "--data_directory",
+        type=str,
+        default="data",
+        help="Prefix for the directory in which the data and vocabulary is stored.",
+    )
+    PARSER.add_argument(
+        "--model_name",
         type=str,
         default="my_own_model",
-        help="Name to pass to the actual train function.",
+        help="Name used for storing intermediate models in the models/ directory.",
     )
-    ARGS = PARSER.parse_args()
-    run(
-        filename_source=ARGS.filename_source,
-        filename_datasets=ARGS.filename_datasets,
-        name=ARGS.name,
-        device=ARGS.device,
+    PARSER.add_argument(
+        "--model_startpoint",
+        type=str,
+        default=None,
+        help="If this is set, a model is loaded from this path as a starting point for training.",
     )
+    PARSER.add_argument(
+        "--num_epochs",
+        type=int,
+        default=10,
+        help="Number of epochs for training.",
+    )
+    PARSER.add_argument(
+        "--batch_size",
+        type=int,
+        default=1,
+        help="Batch size.",
+    )
+    PARSER.add_argument(
+        "--num_training_steps",
+        type=int,
+        default=None,
+        help="Number of steps for training. Overrides --num_epochs if set.",
+    )
+    PARSER.add_argument(
+        "--save_every",
+        type=int,
+        default=2,
+        help="Epochs to complete before saving an intermediate model statedict.",
+    )
+    PARSER.add_argument(
+        "--learning_rate",
+        type=float,
+        default=0.0005,
+        help="Learning rate for the SGD optimizer.",
+    )
+    PARSER.add_argument(
+        "--momentum",
+        type=float,
+        default=0.9,
+        help="Momentum for the SGD optimizer.",
+    )
+    PARSER.add_argument(
+        "--hidden_size",
+        type=int,
+        default=128,
+        help="Hidden size of the recurrent model.",
+    )
+    _run(**vars(PARSER.parse_args()))
