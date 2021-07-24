b1a1ee2c2d9c9a3b8e3f85e124a0f5920f937689
Sasafrass <albertharkema@gmail.com>
2021-07-24 20:52:41 +0200

    Initial update of the meaning, model and word database models.

--- app/models.py
+++ app/models.py
@@ -20,6 +20,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     words = db.relationship("Word", backref="author", lazy="dynamic")
+    meanings = db.relationship("Meaning", backref="author", lazy="dynamic")
     groups = db.relationship(
         "Group",
         secondary=groups,
@@ -49,18 +50,35 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
 
-class Word(db.Model):
+class Meaning(db.Model):
     """Implement a database model for words, their meanings, and their types."""
 
     id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String(56))
-    meaning = db.Column(db.String(280))
-    type = db.Column(db.String(140))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    meaning = db.Column(db.String(140))
+    word_id = db.Column(db.Integer, db.ForeignKey("word.id"))
+
+
+class Word(db.Model):
+    """Implement a database model for words."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(56))
+    model_id = db.Column(db.Integer, db.ForeignKey("model.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    meanings = db.relationship("Meaning", backref="meaning", lazy="dynamic")
 
     def __repr__(self):
         """Instructions on how to display or print a Word database model."""
         return f"<Word {self.word}>"
+
+
+class Model(db.Model):
+    """Implement a database model for the type of model used to generate the word."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(64))
+    words = db.relationship("Word", backref="word", lazy="dynamic")
 
 
 class Group(db.Model):
